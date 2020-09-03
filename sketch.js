5ef1f0a54d509c2c74ebf2f47cf0dc98e215adf7
sparkelyice9 <69832016+sparkelyice9@users.noreply.github.com>
2020-09-02 20:23:25 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var bullet,wall,thickness;
+
+
+var speed, weight; 
+
+
+function setup() {
+  createCanvas(1600, 400);
+
+
+	speed=random(223,321)
+  weight=random(30,52)
+  thickness=random(22,83)
+
+
+	bullet=createSprite(50, 200, 10,5);   
+
+	bullet.velocityX = speed;
+
+	bullet.shapeColor=color("white");
+
+
+  
+  	wall=createSprite(1200,200, thickness, height/2)
+  	wall.shapeColor=color(80,80,80)
+}
+
+
+function draw() {
+  background(0);
+
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2)
+  {
+  	bullet.velocityX=0;
+  	var deformation=0.5 * weight * speed* speed/22509;
+	if(deformation>180)
+	{
+		bullet.shapeColor=color(255,0,0);
+	}
+
+	if(deformation<180 && deformation>100)
+	{
+		bullet.shapeColor=color(230,230,0);
+	}
+
+	if(deformation<100)
+	{
+		bullet.shapeColor=color(0,255,0);
+	}
+  }  
+  
+  drawSprites();
+ 
+}
+
+
